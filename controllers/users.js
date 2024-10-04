8abed854559a406210c2913fe640104fff2f1f77
Nicolas <njcaceres04@gmail.com>
2024-10-03 22:45:19 -0500

    added query params

--- controllers/users.js
+++ controllers/users.js
@@ -1,6 +1,6 @@
 module.exports = {
     get:(req, res) => {
-        res.send(`Get de user ${req.params.id} está sirviendo :D`)
+        res.send(`Get de user ${req.params.id} está sirviendo :D con la query ${req.query.sort_by}`)
     },
     post:(req, res)=>{
         res.send(`Esta es un post request`)
